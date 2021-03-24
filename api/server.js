87e56ba57b74683e9aca3c56dcaf868babb72155
chris westerduin <chriswesterduin@outlook.com>
2021-03-24 17:33:51 +0000

    update seed

--- api/server.js
+++ api/server.js
@@ -5,8 +5,6 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const port = process.env.PORT || 3000;
-
 // Root route
 server.get('/', (req, res) => res.send('Hello, world!'))
 
---
+++ db/2_seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (name, authorid) 
+VALUES
+    ('Harry Potter', '1' ),
+    ('The Shining', '2' ),
+    ('The Hobbit', '3');
+
+INSERT INTO authors (name, birthplace) 
+VALUES
+    ('JK Rowling', 'Scotland' ),
+    ('Stephen King', 'USA' ),
+    ('JRR Tolkein', 'England' );
