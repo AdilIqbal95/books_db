41e9ca0587134c3c7d9c7618b2e014835d333995
chris westerduin <chriswesterduin@outlook.com>
2021-03-24 17:57:55 +0000

    working api and query

--- api/server.js
+++ api/server.js
@@ -8,11 +8,14 @@ server.use(express.json());
 // Root route
 server.get('/', (req, res) => res.send('wow, world!'))
 
+const db = require ('./dbConfig')
+
 function getBooks() {
     return new Promise (async (resolve, reject) => {
         try {
             let bookData = await db.query(`SELECT * FROM books`);
             let book = bookData.rows
+            console.log(book)
             resolve (book);
         } catch (err) {
             reject('books not found');
@@ -21,10 +24,9 @@ function getBooks() {
 }
 
 //all books
-const db = require ('./dbConfig')
 
-server.get('/books', (req,res) => res.send(
-    getBooks()
+server.get('/books', async (req,res) => res.send(
+    await getBooks()
 ))
 
 module.exports = server
