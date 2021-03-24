61e77fcbd54cf79660add139872696f80340dcdc
chris westerduin <chriswesterduin@outlook.com>
2021-03-24 17:51:28 +0000

    db not created error

--- api/server.js
+++ api/server.js
@@ -6,6 +6,25 @@ server.use(cors());
 server.use(express.json());
 
 // Root route
-server.get('/', (req, res) => res.send('Hello, world!'))
+server.get('/', (req, res) => res.send('wow, world!'))
+
+function getBooks() {
+    return new Promise (async (resolve, reject) => {
+        try {
+            let bookData = await db.query(`SELECT * FROM books`);
+            let book = bookData.rows
+            resolve (book);
+        } catch (err) {
+            reject('books not found');
+        }
+    })
+}
+
+//all books
+const db = require ('./dbConfig')
+
+server.get('/books', (req,res) => res.send(
+    getBooks()
+))
 
 module.exports = server
